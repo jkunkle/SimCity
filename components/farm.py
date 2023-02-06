f14f00a37d18c9fd596ab485f3c76d16a4306845
Joshua Kunkle <joshua.kunkle@theventury.com>
2023-02-06 11:55:17 -0500

    add functionality to farm

--- components/farm.py
+++ components/farm.py
@@ -1,12 +1,24 @@
 from components.business import business
 from components.zone import zone_types
 
+import config
+
 class farm(business):
 
-    def __init__(self, total_residences, output_capacity, shape):
+    def __init__(self, curr_residents, output_capacity, shape):
 
+        # init the business part
         super().__init__(zone_types.farm, 0, output_capacity, 0, 0, shape)
 
-        self._total_residences = total_residences
-        self._curr_residences = 0
-        self._residents = []
+        self._output_var = config.FARM_OUTPUT_VAR
+
+
+
+
+        self._output_capacity = curr_residents + config.DEFAULT_EXCESS_OUTPUT_FARM
+        self._curr_residents = curr_residents
+
+
+
+
+
